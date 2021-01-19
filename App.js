601e1e132eb7aca80a71170a23d79622f1701e43
shander3377 <pankaj3377@gmail.com>
2021-01-19 17:22:58 +0530

    updates

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SignupLoginScreen from './screens/SignupLoginScreen';
-
+import {createBottomTabNavigator, r} from 'react-navigation-tabs'
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import ExchangeScreen from "./screens/ExchangeScreen.js"
+import HomeScreen from "./screens/HomeScreen.js"
 export default class App extends React.Components {
   render() {
     return (
@@ -11,3 +14,14 @@ export default class App extends React.Components {
     );
   }
 }
+const tabNavigator = createBottomTabNavigator({
+  ExchangeScreen : {screen: ExchangeScreen},
+  HomeScreen : {screen: HomeScreen},
+  })
+  const switchNavigator = createSwitchNavigator({
+    SignupLoginScreen: {screen: SignupLoginScreen},
+     tabNavigator: {screen: tabNavigator}
+  
+  })
+  const AppContainer = createAppContainer(switchNavigator)
+  
---
+++ screens/HomeScreen.js
@@ -0,0 +1,194 @@
+import React from "react";
+import {
+  Modal,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+} from "react-native";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import db from "../config.js";
+import firebase from "firebase";
+import MyHeader from '../components/MyHeader';
+import { ListItem } from 'react-native-elements'
+export default class HomeScreen extends React.Components {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allRequests: []
+    };
+    this.requestRef=null
+  }
+     
+
+  
+  getAllRequests =()=>{
+     this.requestRef = db.collection("exchange_requests")
+    .onSnapshot((snapshot)=>{
+      var allRequests = []
+      snapshot.forEach((doc) => {
+          allRequests.push(doc.data())
+      })
+      this.setState({allRequests:allRequests})
+    })
+  }
+  componentDidMount = () =>{
+    this.requestRef()
+  }
+  componentWillUnmount = () =>{
+    this.requestRef()
+  }
+
+
+  keyExtractor = (item, index) => index.toString()
+
+  renderItem = ( {item, i} ) =>{
+    return (
+      <ListItem
+        key={i}
+        title={item.item_name}
+        subtitle={item.description}
+        titleStyle={{ color: 'black', fontWeight: 'bold' }}
+        rightElement={
+            <TouchableOpacity style={styles.button}>
+              <Text style={{color:'#ffff'}}>Exchange</Text>
+            </TouchableOpacity>
+          }
+        bottomDivider
+      />
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <View style={{flex:1}}>
+          {
+            this.state.allRequests.length === 0
+            ?(
+              <View style={styles.subContainer}>
+                <Text style={{ fontSize: 20}}>List Of All Requested Items</Text>
+              </View>
+            )
+            :(
+              <FlatList
+                keyExtractor={this.keyExtractor}
+                data={this.state.allRequests}
+                renderItem={this.renderItem}
+              />
+            )
+          }
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+   flex:1,
+   backgroundColor:'#F8BE85',
+   alignItems: 'center',
+   justifyContent: 'center'
+ },
+ profileContainer:{
+   flex:1,
+   justifyContent:'center',
+   alignItems:'center',
+ },
+ title :{
+   fontSize:65,
+   fontWeight:'300',
+   paddingBottom:30,
+   color : '#ff3d00'
+ },
+ loginBox:{
+   width: 300,
+   height: 40,
+   borderBottomWidth: 1.5,
+   borderColor : '#ff8a65',
+   fontSize: 20,
+   margin:10,
+   paddingLeft:10
+ },
+ KeyboardAvoidingView:{
+   flex:1,
+   justifyContent:'center',
+   alignItems:'center'
+ },
+ modalTitle :{
+   justifyContent:'center',
+   alignSelf:'center',
+   fontSize:30,
+   color:'#ff5722',
+   margin:50
+ },
+ modalContainer:{
+   flex:1,
+   borderRadius:20,
+   justifyContent:'center',
+   alignItems:'center',
+   backgroundColor:"#ffff",
+   marginRight:30,
+   marginLeft : 30,
+   marginTop:80,
+   marginBottom:80,
+ },
+ formTextInput:{
+   width:"75%",
+   height:35,
+   alignSelf:'center',
+   borderColor:'#ffab91',
+   borderRadius:10,
+   borderWidth:1,
+   marginTop:20,
+   padding:10
+ },
+ registerButton:{
+   width:200,
+   height:40,
+   alignItems:'center',
+   justifyContent:'center',
+   borderWidth:1,
+   borderRadius:10,
+   marginTop:30
+ },
+ registerButtonText:{
+   color:'#ff5722',
+   fontSize:15,
+   fontWeight:'bold'
+ },
+ cancelButton:{
+   width:200,
+   height:30,
+   justifyContent:'center',
+   alignItems:'center',
+   marginTop:5,
+ },
+
+ button:{
+   width:300,
+   height:50,
+   justifyContent:'center',
+   alignItems:'center',
+   borderRadius:25,
+   backgroundColor:"#ff9800",
+   shadowColor: "#000",
+   shadowOffset: {
+      width: 0,
+      height: 8,
+   },
+   shadowOpacity: 0.30,
+   shadowRadius: 10.32,
+   elevation: 16,
+   padding: 10
+ },
+ buttonText:{
+   color:'#ffff',
+   fontWeight:'200',
+   fontSize:20
+ }
+})
+
