8cb2a3470ecf7f917d831cc7c7a4adb34197e026
shander3377 <pankaj3377@gmail.com>
2021-01-30 21:09:00 +0530

    updates

--- App.js
+++ App.js
@@ -5,11 +5,14 @@ import {createBottomTabNavigator, r} from 'react-navigation-tabs'
 import {createAppContainer, createSwitchNavigator} from 'react-navigation'
 import ExchangeScreen from "./screens/ExchangeScreen.js"
 import HomeScreen from "./screens/HomeScreen.js"
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {SettingsScreen} from "../screens/SettingsScreen.js"
+import CustomSideBarMenu  from './CustomSideBarMenu';
 export default class App extends React.Components {
   render() {
     return (
       <View>
-    <SignupLoginScreen/>
+    <AppContainer/>
   </View>
     );
   }
@@ -20,8 +23,22 @@ const tabNavigator = createBottomTabNavigator({
   })
   const switchNavigator = createSwitchNavigator({
     SignupLoginScreen: {screen: SignupLoginScreen},
-     tabNavigator: {screen: tabNavigator}
-  
+     tabNavigator: {screen: tabNavigator},
+  drawerNavigator: {screen: sideNavigator}
   })
+  const sideNavigator = createDrawerNavigator({
+    Home : {
+      screen : tabNavigator
+      },
+      Setting:{
+        screen: SettingsScreen
+      }
+    },
+    {
+      contentComponent:CustomSideBarMenu
+    },
+    {
+      initialRouteName : 'Home'
+    })
   const AppContainer = createAppContainer(switchNavigator)
   
